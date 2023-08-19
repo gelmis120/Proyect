7d94c9dec8b47b8c5cdfe5221a5a9eb1e2bffae1
Gelmis <av835732@gmail.com>
2023-08-19 00:07:01 -0800

    Este es el script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    // Simulate login logic (replace with actual authentication)
+    if (username === "user" && password === "password") {
+      alert("Login successful!");
+    } else {
+      alert("Login failed. Please check your credentials.");
+    }
+  }
+  
